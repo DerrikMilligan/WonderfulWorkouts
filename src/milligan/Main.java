b0bd5051b24f10a9b3a6ed4e7b411beea7cc7e95
Emily Purser <pur20008@byui.edu>
2021-05-17 21:24:23 -0600

    Said hello to Derrik

--- src/milligan/Main.java
+++ src/milligan/Main.java
@@ -3,6 +3,8 @@ package milligan;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello from Derrik");
+        System.out.pringln("Hello Derrik, my name is Emily");
     }
 }
