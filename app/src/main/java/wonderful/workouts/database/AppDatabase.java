46a30a00dc0ea936999f2c1923fdb80d241ddd47
Derrik Milligan <derrik.milligan.dev@gmail.com>
2021-06-15 21:19:26 -0600

    Forgot to add all the Entities and Daos to the Database class.

--- app/src/main/java/wonderful/workouts/database/AppDatabase.java
+++ app/src/main/java/wonderful/workouts/database/AppDatabase.java
@@ -7,22 +7,47 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
+import wonderful.workouts.database.daos.MeasurementDao;
+import wonderful.workouts.database.daos.MovementDao;
 import wonderful.workouts.database.daos.UserDao;
+import wonderful.workouts.database.daos.WorkoutDao;
+import wonderful.workouts.database.daos.WorkoutHistoryDao;
+import wonderful.workouts.database.daos.WorkoutMovementCrossRefDao;
+import wonderful.workouts.database.daos.WorkoutMovementHistoryDao;
+import wonderful.workouts.database.entities.Measurement;
+import wonderful.workouts.database.entities.Movement;
 import wonderful.workouts.database.entities.User;
+import wonderful.workouts.database.entities.Workout;
+import wonderful.workouts.database.entities.WorkoutHistory;
+import wonderful.workouts.database.entities.WorkoutMovementCrossRef;
+import wonderful.workouts.database.entities.WorkoutMovementHistory;
 
 @Database(
     version = 1,
     // Here we list out all the entities we'll be using
     entities = {
+        Measurement.class,
+        Movement.class,
         User.class,
+        Workout.class,
+        WorkoutHistory.class,
+        WorkoutMovementCrossRef.class,
+        WorkoutMovementHistory.class,
     }
 )
+// The TypeConverts help us convert the Java date type to something the database can handle and back
 @TypeConverters({ Converters.class })
 abstract class AppDatabase extends RoomDatabase {
     // Here we list out all the DAO and the methods will be built by the @Database wrapper for us
+    abstract public MeasurementDao getMeasurementDao();
+    abstract public MovementDao getMovementDao();
     abstract public UserDao getUserDao();
+    abstract public WorkoutDao getWorkoutDao();
+    abstract public WorkoutHistoryDao getWorkoutHistoryDao();
+    abstract public WorkoutMovementCrossRefDao getWorkoutMovementCrossRefDao();
+    abstract public WorkoutMovementHistoryDao getWorkoutMovementHistoryDao();
 
-    // This will be the static instance of the database so we only ever have one connection. This is the singleton pattern
+    // This will be the static instance of the database so we only ever have one connection. This follows the singleton pattern
     // The volatile keyword ensure that if this is changed it'll be updated across threads
     volatile private static AppDatabase INSTANCE = null;
 
